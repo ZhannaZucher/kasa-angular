5fa77118326ff77feb6cb75b2d85b41fc3e2e004
ZhannaZucher <jzucher@gmail.com>
2024-03-07 16:54:05 +0100

    feat: configure router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router"
+import { HomePageComponent } from "./home-page/home-page.component"
+import { AboutPageComponent } from "./about-page/about-page.component"
+import { AccomodationPageComponent } from "./accomodation-page/accomodation-page.component"
+import { ErrorPageComponent } from "./error-page/error-page.component"
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: HomePageComponent },
+  { path: "about", component: AboutPageComponent },
+  { path: "accomodation/:id", component: AccomodationPageComponent },
+  { path: "**", pathMatch: "full", component: ErrorPageComponent },
+]
