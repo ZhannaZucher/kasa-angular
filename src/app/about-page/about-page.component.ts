bf1e68d8c5e20ab8795cd82b759f0e73acba8218
ZhannaZucher <jzucher@gmail.com>
2024-03-08 18:08:37 +0100

    add: collapse to about page

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -1,11 +1,36 @@
 import { Component } from "@angular/core"
 import { BannerComponent } from "../banner/banner.component"
+import { CollapseComponent } from "../collapse/collapse.component"
+import { NgFor } from "@angular/common"
 
 @Component({
   selector: "app-about-page",
   standalone: true,
-  imports: [BannerComponent],
+  imports: [BannerComponent, CollapseComponent, NgFor],
   templateUrl: "./about-page.component.html",
   styleUrl: "./about-page.component.css",
 })
-export class AboutPageComponent {}
+export class AboutPageComponent {
+  dataAbout = [
+    {
+      title: "Fiabilité",
+      description:
+        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
+    },
+    {
+      title: "Respect",
+      description:
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+    },
+    {
+      title: "Service",
+      description:
+        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+    },
+    {
+      title: "Sécurité",
+      description:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ]
+}
