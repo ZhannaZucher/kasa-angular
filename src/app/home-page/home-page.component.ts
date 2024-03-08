94779a0da3c7a4348b0ddde31c6e895f7a9c85bf
ZhannaZucher <jzucher@gmail.com>
2024-03-08 17:17:29 +0100

    add : accomodations' views

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,23 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
 import { BannerComponent } from "../banner/banner.component"
+import { NgFor } from "@angular/common"
+import { Accomodation } from "../models/accomodation.model"
+import { AccomodationsService } from "../services/accomodations.service"
+import { AccomodationViewComponent } from "../accomodation-view/accomodation-view.component"
 
 @Component({
   selector: "app-home-page",
   standalone: true,
-  imports: [BannerComponent],
+  imports: [NgFor, BannerComponent, AccomodationViewComponent],
   templateUrl: "./home-page.component.html",
   styleUrl: "./home-page.component.css",
 })
-export class HomePageComponent {}
+export class HomePageComponent implements OnInit {
+  accomodations!: Accomodation[]
+
+  constructor(private AccomodationsService: AccomodationsService) {}
+
+  ngOnInit(): void {
+    this.accomodations = this.AccomodationsService.getAllAccomodations()
+  }
+}
