2083277c3ea87718a92c23403add2c4fee1f5108
ZhannaZucher <jzucher@gmail.com>
2024-03-09 17:11:31 +0100

    add: gallery to the page

--- src/app/accomodation-page/accomodation-page.component.ts
+++ src/app/accomodation-page/accomodation-page.component.ts
@@ -3,11 +3,12 @@ import { Accomodation } from "../models/accomodation.model"
 import { ActivatedRoute, RouterModule } from "@angular/router"
 import { AccomodationsService } from "../services/accomodations.service"
 import { AccomodationCardComponent } from "../accomodation-card/accomodation-card.component"
+import { GalleryComponent } from "../gallery/gallery.component"
 
 @Component({
   selector: "app-accomodation-page",
   standalone: true,
-  imports: [RouterModule, AccomodationCardComponent],
+  imports: [RouterModule, AccomodationCardComponent, GalleryComponent],
   templateUrl: "./accomodation-page.component.html",
   styleUrl: "./accomodation-page.component.css",
 })
