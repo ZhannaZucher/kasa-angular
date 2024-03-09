9512eb84c82d21263c6e7782b3e6e58772836640
ZhannaZucher <jzucher@gmail.com>
2024-03-09 08:35:08 +0100

    add: tag component

--- src/app/accomodation-card/accomodation-card.component.ts
+++ src/app/accomodation-card/accomodation-card.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from "@angular/core"
 import { Accomodation } from "../models/accomodation.model"
 import { CollapseComponent } from "../collapse/collapse.component"
 import { NgFor } from "@angular/common"
+import { AccomodationTagsComponent } from "../accomodation-tags/accomodation-tags.component"
 
 @Component({
   selector: "app-accomodation-card",
   standalone: true,
-  imports: [CollapseComponent, NgFor],
+  imports: [CollapseComponent, NgFor, AccomodationTagsComponent],
   templateUrl: "./accomodation-card.component.html",
   styleUrl: "./accomodation-card.component.css",
 })
---
+++ src/app/accomodation-tags/accomodation-tags.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accomodation-tags',
+  standalone: true,
+  imports: [],
+  templateUrl: './accomodation-tags.component.html',
+  styleUrl: './accomodation-tags.component.css'
+})
+export class AccomodationTagsComponent {
+
+}
