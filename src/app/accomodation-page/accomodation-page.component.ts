06a499a5a2f889241ad1f98b92af4dd8a1d12d52
ZhannaZucher <jzucher@gmail.com>
2024-03-09 08:28:20 +0100

    add: accomodation card to page

---
+++ src/app/accomodation-page/accomodation-page.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from "@angular/core"
+import { Accomodation } from "../models/accomodation.model"
+import { ActivatedRoute, RouterModule } from "@angular/router"
+import { AccomodationsService } from "../services/accomodations.service"
+import { log } from "console"
+import { AccomodationCardComponent } from "../accomodation-card/accomodation-card.component"
+
+@Component({
+  selector: "app-accomodation-page",
+  standalone: true,
+  imports: [RouterModule, AccomodationCardComponent],
+  templateUrl: "./accomodation-page.component.html",
+  styleUrl: "./accomodation-page.component.css",
+})
+export class AccomodationPageComponent implements OnInit {
+  accomodation!: Accomodation
+
+  constructor(
+    private AccomodationsService: AccomodationsService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const accId = this.route.snapshot.params["id"]
+    this.accomodation = this.AccomodationsService.getAccomodationById(accId)
+  }
+}
